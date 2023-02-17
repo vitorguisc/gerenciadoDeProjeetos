302dc6d8c28fa383513ab277f0ad8372536f7e97
Vitor <vitorguilherme@geb.inatel.br>
2023-02-16 21:13:52 -0300

    Envio com bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { BrowserRouter as Router, Routes, Route, Container, Link } from "react-router-dom";
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
 function App() {
   return (
-    <div className="App">
-        <h1>Costs</h1>    
-    </div>
+    <Container>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newprojeect">Novo Projeto</Link>
+      </div>
+      
+      <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/company">
+          <Company />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/newproject">
+          <NewProject />
+        </Route>
+      </Routes>
+     
+      <p>Footer</p>
+    </Router>
+    </Container>
   );
 }
 
